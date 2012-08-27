975a373987404a299c9c803370dee33d56c33ce2
jakedahn <jake@markupisart.com>
2012-08-27 00:57:25 -0700

    locking down posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,51 +22,33 @@ class PostsController < ApplicationController
 
 
   def new
-    title = "Untitled Post #{Time.now.strftime("%m/%d/%Y")}"
-    @post = Post.create(title: title,
-                        raw_body: "!!## #{title} ##!!",
-                        draft: true)
-    redirect_to edit_post_path(@post)
-  end
-
-
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-
-  def create
-    @post = Post.new(params[:post])
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to edit_post_url(@post), notice: 'Post was successfully created.' }
-        format.json { render json: @post, status: :created, location: @post }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if current_user
+      title = "Untitled Post #{Time.now.strftime("%m/%d/%Y")}"
+      @post = Post.create(title: title,
+                          raw_body: "!!## #{title} ##!!",
+                          draft: true)
+      redirect_to edit_post_path(@post)
     end
   end
 
 
-  def update
-    @post = Post.find(params[:id])
+  def edit
+    if current_user
+      @post = Post.find(params[:id])
+    end
+  end
 
-    raw_body = params[:post][:raw_body]
 
-    if @post.raw_body != raw_body
+  def create
+    if current_user
+      @post = Post.new(params[:post])
+
       respond_to do |format|
-        post_title = parse_title(raw_body)
-        raw_body_minus_title = raw_body.split("\#\#\!\!\n")[1]
-
-        if @post.update_attributes(raw_body: raw_body,
-                                   cached_body: markdown(raw_body_minus_title),
-                                   title: post_title)
-          format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-          format.json { head :no_content }
+        if @post.save
+          format.html { redirect_to edit_post_url(@post), notice: 'Post was successfully created.' }
+          format.json { render json: @post, status: :created, location: @post }
         else
-          format.html { render action: "edit" }
+          format.html { render action: "new" }
           format.json { render json: @post.errors, status: :unprocessable_entity }
         end
       end
@@ -74,15 +56,42 @@ class PostsController < ApplicationController
   end
 
 
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
+  def update
+    if current_user
+      @post = Post.find(params[:id])
 
-    respond_to do |format|
-      format.html { redirect_to posts_url }
-      format.json { head :no_content }
+      raw_body = params[:post][:raw_body]
+
+      if @post.raw_body != raw_body
+        respond_to do |format|
+          post_title = parse_title(raw_body)
+          raw_body_minus_title = raw_body.split("\#\#\!\!\n")[1]
+
+          if @post.update_attributes(raw_body: raw_body,
+                                     cached_body: markdown(raw_body_minus_title),
+                                     title: post_title)
+            format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+            format.json { head :no_content }
+          else
+            format.html { render action: "edit" }
+            format.json { render json: @post.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
     end
   end
+
+
+  # def destroy
+  #   @post = Post.find(params[:id])
+  #   @post.destroy
+
+  #   respond_to do |format|
+  #     format.html { redirect_to posts_url }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
 
 def feed
